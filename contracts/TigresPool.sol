34f57ae39a6349f7069928090805a634ebfed419
sarisssa <mikechansas@gmail.com>
2023-01-16 12:53:56 -0800

    feat: implement liquidity pool functionality

--- contracts/TigresPool.sol
+++ contracts/TigresPool.sol
@@ -71,6 +71,54 @@ contract TigresPool is Initializable, ERC20Upgradeable, OwnableUpgradeable, UUPS
 
     }
 
+    function addLiquidity(uint256 _amount0, uint256 _amount1)
+        external
+        returns (uint256 poolTokens)
+
+    {
+        token0.transferFrom(msg.sender, address(this), _amount0);
+        token1.transferFrom(msg.sender, address(this), _amount1);
+
+        if (poolTokenReserve == 0) {
+            poolTokens = _sqrt(_amount0 * _amount1);
+        } else {
+            poolTokens = _min(
+                (_amount0 * poolTokenReserve) / reserve0,
+                (_amount1 * poolTokenReserve) / reserve1
+            );
+        }
+        require(
+            poolTokens > 0,
+            "Amount of Pool Tokens being rewarded must be greater than 0."
+        );
+
+        _mint(msg.sender, poolTokens);
+
+        _updatePool(
+            token0.balanceOf(address(this)),
+            token1.balanceOf(address(this))
+        );
+    }
+     
+    function removeLiquidity(uint256 _shares)
+        external
+        returns (uint256 withdrawAmount0, uint256 withdrawAmount1)
+    {
+
+        uint256 poolBalance0 = token0.balanceOf(address(this));
+        uint256 poolBalance1 = token1.balanceOf(address(this));
+
+        withdrawAmount0 = (_shares * poolBalance0) / poolTokenReserve;
+        withdrawAmount1 = (_shares * poolBalance1) / poolTokenReserve;
+        require(withdrawAmount0 > 0 && withdrawAmount1 > 0, "withdrawAmount0 or withdrawAmount1 = 0");
+
+        _burn(msg.sender, _shares);
+        _updatePool(poolBalance0 - withdrawAmount0, poolBalance1 - withdrawAmount1);
+
+        token0.transfer(msg.sender, withdrawAmount0);
+        token1.transfer(msg.sender, withdrawAmount1);
+    }
+
     function initialize() initializer public {
         __ERC20_init("poolTigres", "pTG");
         __Ownable_init();
@@ -89,5 +137,22 @@ contract TigresPool is Initializable, ERC20Upgradeable, OwnableUpgradeable, UUPS
     function _updatePool(uint256 _reserve0, uint256 _reserve1) private {
         reserve0 = _reserve0;
         reserve1 = _reserve1;
+    }
+
+    function _sqrt(uint256 y) private pure returns (uint256 z) {
+        if (y > 3) {
+            z = y;
+            uint256 x = y / 2 + 1;
+            while (x < z) {
+                z = x;
+                x = (y / x + x) / 2;
+            }
+        } else if (y != 0) {
+            z = 1;
+        }
+    }
+
+    function _min(uint256 x, uint256 y) private pure returns (uint256) {
+        return x <= y ? x : y;
     }
 }

52304b6f3306b14c5cf7c7934d7455a9d07e6049
sarisssa <mikechansas@gmail.com>
2023-01-16 01:29:33 -0800

    feat: implement unit tests for invalid swap inputs

--- test/Lock.js
+++ test/Lock.js
@@ -1,126 +1,61 @@
-const {
-  time,
-  loadFixture,
-} = require("@nomicfoundation/hardhat-network-helpers");
-const { anyValue } = require("@nomicfoundation/hardhat-chai-matchers/withArgs");
+const { loadFixture } = require("@nomicfoundation/hardhat-network-helpers");
 const { expect } = require("chai");
+const { ethers } = require("hardhat");
 
-describe("Lock", function () {
-  // We define a fixture to reuse the same setup in every test.
-  // We use loadFixture to run this setup once, snapshot that state,
-  // and reset Hardhat Network to that snapshot in every test.
-  async function deployOneYearLockFixture() {
-    const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-    const ONE_GWEI = 1_000_000_000;
+describe("Tigres Pool Contract", function () {
+  async function deployTigresPoolFixture() {
+    const accounts = await ethers.getSigners();
+    const randomAddresses = [
+      accounts[0].address,
+      accounts[1].address,
+      accounts[2].address,
+    ];
+    const esperTokenFactory = await ethers.getContractFactory("Esper");
+    const esperToken = await esperTokenFactory.deploy();
+    const esperTokenAddress = esperToken.address;
 
-    const lockedAmount = ONE_GWEI;
-    const unlockTime = (await time.latest()) + ONE_YEAR_IN_SECS;
+    const azoriusTokenFactory = await ethers.getContractFactory("Azorius");
+    const azoriusToken = await azoriusTokenFactory.deploy();
+    const azoriusTokenAddress = azoriusToken.address;
 
-    // Contracts are deployed using the first signer/account by default
-    const [owner, otherAccount] = await ethers.getSigners();
+    const tigresPoolFactory = await ethers.getContractFactory("TigresPool");
 
-    const Lock = await ethers.getContractFactory("Lock");
-    const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+    const tigresPoolInstance = await tigresPoolFactory.deploy(
+      esperTokenAddress,
+      azoriusTokenAddress
+    );
+    let res = await esperToken.approve(tigresPoolInstance.address, 1000);
+    console.log(res);
+    let result = await esperToken._allowances();
+    // console.log(await tigresPoolInstance.reserve0());
 
-    return { lock, unlockTime, lockedAmount, owner, otherAccount };
+    return {
+      randomAddresses,
+      esperToken,
+      esperTokenAddress,
+      azoriusTokenAddress,
+      tigresPoolInstance,
+    };
   }
 
-  describe("Deployment", function () {
-    it("Should set the right unlockTime", async function () {
-      const { lock, unlockTime } = await loadFixture(deployOneYearLockFixture);
-
-      expect(await lock.unlockTime()).to.equal(unlockTime);
-    });
-
-    it("Should set the right owner", async function () {
-      const { lock, owner } = await loadFixture(deployOneYearLockFixture);
-
-      expect(await lock.owner()).to.equal(owner.address);
-    });
-
-    it("Should receive and store the funds to lock", async function () {
-      const { lock, lockedAmount } = await loadFixture(
-        deployOneYearLockFixture
-      );
-
-      expect(await ethers.provider.getBalance(lock.address)).to.equal(
-        lockedAmount
-      );
-    });
-
-    it("Should fail if the unlockTime is not in the future", async function () {
-      // We don't use the fixture here because we want a different deployment
-      const latestTime = await time.latest();
-      const Lock = await ethers.getContractFactory("Lock");
-      await expect(Lock.deploy(latestTime, { value: 1 })).to.be.revertedWith(
-        "Unlock time should be in the future"
-      );
-    });
+  it("Should revert when an invalid address is passed to the swap function", async function () {
+    const { randomAddresses, tigresPoolInstance } = await loadFixture(
+      deployTigresPoolFixture
+    );
+    const randomAddress = randomAddresses[0];
+    await expect(
+      tigresPoolInstance.swap(randomAddress, 1000)
+    ).to.be.revertedWith("Invalid token being swapped");
   });
 
-  describe("Withdrawals", function () {
-    describe("Validations", function () {
-      it("Should revert with the right error if called too soon", async function () {
-        const { lock } = await loadFixture(deployOneYearLockFixture);
-
-        await expect(lock.withdraw()).to.be.revertedWith(
-          "You can't withdraw yet"
-        );
-      });
-
-      it("Should revert with the right error if called from another account", async function () {
-        const { lock, unlockTime, otherAccount } = await loadFixture(
-          deployOneYearLockFixture
-        );
-
-        // We can increase the time in Hardhat Network
-        await time.increaseTo(unlockTime);
-
-        // We use lock.connect() to send a transaction from another account
-        await expect(lock.connect(otherAccount).withdraw()).to.be.revertedWith(
-          "You aren't the owner"
-        );
-      });
-
-      it("Shouldn't fail if the unlockTime has arrived and the owner calls it", async function () {
-        const { lock, unlockTime } = await loadFixture(
-          deployOneYearLockFixture
-        );
-
-        // Transactions are sent using the first signer by default
-        await time.increaseTo(unlockTime);
-
-        await expect(lock.withdraw()).not.to.be.reverted;
-      });
-    });
-
-    describe("Events", function () {
-      it("Should emit an event on withdrawals", async function () {
-        const { lock, unlockTime, lockedAmount } = await loadFixture(
-          deployOneYearLockFixture
-        );
-
-        await time.increaseTo(unlockTime);
-
-        await expect(lock.withdraw())
-          .to.emit(lock, "Withdrawal")
-          .withArgs(lockedAmount, anyValue); // We accept any value as `when` arg
-      });
-    });
-
-    describe("Transfers", function () {
-      it("Should transfer the funds to the owner", async function () {
-        const { lock, unlockTime, lockedAmount, owner } = await loadFixture(
-          deployOneYearLockFixture
-        );
-
-        await time.increaseTo(unlockTime);
-
-        await expect(lock.withdraw()).to.changeEtherBalances(
-          [owner, lock],
-          [lockedAmount, -lockedAmount]
-        );
-      });
-    });
+  it("Should revert when the swap function is called with 0 amount of tokens", async function () {
+    const { esperTokenAddress, tigresPoolInstance } = await loadFixture(
+      deployTigresPoolFixture
+    );
+    await expect(
+      tigresPoolInstance.swap(esperTokenAddress, 0)
+    ).to.be.revertedWith(
+      "Amount of token being swapped must be greater than 0"
+    );
   });
 });
